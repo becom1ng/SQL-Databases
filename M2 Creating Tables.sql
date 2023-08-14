d864152ca4d33ddf98d7fcac71e444507b557340
becom1ng <becom1ng@users.noreply.github.com>
2023-08-14 18:17:05 -0500

    M2: Added semicolons

--- M2 Creating Tables.sql
+++ M2 Creating Tables.sql
@@ -6,7 +6,7 @@ create table Customer (
 	LastName varchar(80) NOT NULL,
 	Birthdate date,
 	Email varchar(80) NOT NULL,
-)
+);
 
 create table [Order]  (
     OrderId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
@@ -22,11 +22,11 @@ create table Product (
 	ProductId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	ProductName varchar(100) NOT NULL,
 	Price decimal NOT NULL
-)
+);
 
 create table OrderItem (
 	OrderId int NOT NULL FOREIGN KEY REFERENCES [Order](OrderId),
 	ProductId int NOT NULL FOREIGN KEY REFERENCES Product(ProductId),
 	Price decimal NOT NULL,
 	Quantity int NOT NULL
-)
+);
