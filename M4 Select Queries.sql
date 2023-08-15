8b1154a621201c68850f275762e393bcc3cff76c
becom1ng <becom1ng@users.noreply.github.com>
2023-08-15 10:58:45 -0500

    M4: Update to add USE to each query.

--- M4 Select Queries.sql
+++ M4 Select Queries.sql
@@ -1,15 +1,17 @@
 Use SimpleOrder;
-
 SELECT CustomerId, FirstName, LastName FROM Customer
 	WHERE FirstName = 'Ryan';
 
+Use SimpleOrder;
 SELECT CustomerId, FirstName, LastName, Birthdate FROM Customer
 	WHERE LastName LIKE '%Lear%';
 
 -- AGE > 25 years. Not 100% accurate due to datediff.
+Use SimpleOrder;
 SELECT FirstName, Birthdate FROM Customer
 	WHERE DATEDIFF(DAY, Birthdate, GETDATE())/365.25 > 25;
 
 -- AGE > 25 & < 45. Not 100% accurate due to datediff.
+Use SimpleOrder;
 SELECT * FROM Customer
 	WHERE DATEDIFF(DAY, Birthdate, GETDATE())/365.25 > 25 AND DATEDIFF(DAY, Birthdate, GETDATE())/365.25 < 45;
